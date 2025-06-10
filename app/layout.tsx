c90d9d3e8319fd1ac7bd2b4350d8f8c0aa68313a
Al Aqmar <alaqmarak0810@gmail.com>
2025-06-10 11:50:56 +0530

    updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.jpg" />
+        <link rel="apple-touch-icon" href="/logo.jpg" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased mt-3 flex flex-col items-center justify-center`}
       >
